3dcfb51e569721325a0ab20a7d17a25e9b0afb72
Rudwolfr <Rudwolfr@users.noreply.github.com>
2018-07-05 12:19:11 -0500

    Update activity.php
    Traducción

--- admin/language/es-ES/extension/dashboard/activity.php
+++ admin/language/es-ES/extension/dashboard/activity.php
@@ -1,32 +1,32 @@
 <?php
 // Heading
-$_['heading_title']                = 'Recent Activity';
+$_['heading_title']                = 'Actividad Reciente';
 
 // Text
-$_['text_extension']               = 'Extensions';
-$_['text_success']                 = 'Success: You have modified dashboard activity!';
-$_['text_edit']                    = 'Edit Dashboard Recent Activity';
-$_['text_activity_register']       = '<a href="customer_id=%d">%s</a> registered a new account.';
-$_['text_activity_edit']           = '<a href="customer_id=%d">%s</a> updated their account details.';
-$_['text_activity_password']       = '<a href="customer_id=%d">%s</a> updated their account password.';
-$_['text_activity_reset']          = '<a href="customer_id=%d">%s</a> reset their account password.';
-$_['text_activity_login']          = '<a href="customer_id=%d">%s</a> logged in.';
-$_['text_activity_forgotten']      = '<a href="customer_id=%d">%s</a> has requested a reset password.';
-$_['text_activity_address_add']    = '<a href="customer_id=%d">%s</a> added a new address.';
-$_['text_activity_address_edit']   = '<a href="customer_id=%d">%s</a> updated their address.';
-$_['text_activity_address_delete'] = '<a href="customer_id=%d">%s</a> deleted one of their addresses.';
-$_['text_activity_return_account'] = '<a href="customer_id=%d">%s</a> submitted a product <a href="return_id=%d">return</a>.';
-$_['text_activity_return_guest']   = '%s submitted a product <a href="return_id=%d">return</a>.';
-$_['text_activity_order_account']  = '<a href="customer_id=%d">%s</a> added a <a href="order_id=%d">new order</a>.';
-$_['text_activity_order_guest']    = '%s created a <a href="order_id=%d">new order</a>.';
-$_['text_activity_affiliate_add']  = '<a href="customer_id=%d">%s</a> registered for a affiliate account.';
-$_['text_activity_affiliate_edit'] = '<a href="customer_id=%d">%s</a> updated their affiliate details.';
-$_['text_activity_transaction']    = '<a href="customer_id=%d">%s</a> received commission from an new <a href="order_id=%d">order</a>.';
+$_['text_extension']               = 'Extensiones';
+$_['text_success']                 = 'Éxito: Ha modificado la actividad del Tablero!';
+$_['text_edit']                    = 'Editar Actividad Reciente del Tablero';
+$_['text_activity_register']       = '<a href="customer_id=%d">%s</a> registrar una nueva cuenta.';
+$_['text_activity_edit']           = '<a href="customer_id=%d">%s</a> actualizar los detalles de su cuenta.';
+$_['text_activity_password']       = '<a href="customer_id=%d">%s</a> actualizó la contraseña de su cuenta.';
+$_['text_activity_reset']          = '<a href="customer_id=%d">%s</a> reestrableció su contraseña de la cuenta.';
+$_['text_activity_login']          = '<a href="customer_id=%d">%s</a> Loguearse.';
+$_['text_activity_forgotten']      = '<a href="customer_id=%d">%s</a> ha solicitado un reinicio de contraseña.';
+$_['text_activity_address_add']    = '<a href="customer_id=%d">%s</a> agregar nueva dirección.';
+$_['text_activity_address_edit']   = '<a href="customer_id=%d">%s</a> actualizar su domicilio.';
+$_['text_activity_address_delete'] = '<a href="customer_id=%d">%s</a> borrar uno de sus domicilios.';
+$_['text_activity_return_account'] = '<a href="customer_id=%d">%s</a> enviado un producto para <a href="return_id=%d">devolución</a>.';
+$_['text_activity_return_guest']   = '%s enviado un producto para <a href="return_id=%d">devolución</a>.';
+$_['text_activity_order_account']  = '<a href="customer_id=%d">%s</a> agregó una <a href="order_id=%d">nuevo pedido</a>.';
+$_['text_activity_order_guest']    = '%s creó un <a href="order_id=%d">nuevo pedido</a>.';
+$_['text_activity_affiliate_add']  = '<a href="customer_id=%d">%s</a> registró una cuenta afiliada.';
+$_['text_activity_affiliate_edit'] = '<a href="customer_id=%d">%s</a> actualizó sus detalles de afiliado.';
+$_['text_activity_transaction']    = '<a href="customer_id=%d">%s</a> recibió comisión de una nuevo <a href="order_id=%d">pedido</a>.';
 
 // Entry
-$_['entry_status']                 = 'Status';
-$_['entry_sort_order']             = 'Sort Order';
-$_['entry_width']                  = 'Width';
+$_['entry_status']                 = 'Estado';
+$_['entry_sort_order']             = 'Orden de Aparición';
+$_['entry_width']                  = 'Ancho';
 
 // Error
-$_['error_permission']             = 'Warning: You do not have permission to modify dashboard activity!';
+$_['error_permission']             = 'Advertencia: ¡Usted no tiene permiso para modificar la actividad del tablero!';
