12f37921ade67244636d65c9b109d8c7a621cc7f
Rudwolfr <Rudwolfr@users.noreply.github.com>
2018-07-18 09:29:40 -0500

    Update free_checkout.php
    Traducción

--- admin/language/es-ES/extension/payment/free_checkout.php
+++ admin/language/es-ES/extension/payment/free_checkout.php
@@ -1,16 +1,16 @@
 <?php
 // Heading
-$_['heading_title']      = 'Free Checkout';
+$_['heading_title']      = 'Pago Gratis';
 
 // Text
-$_['text_extension']     = 'Extensions';
-$_['text_success']       = 'Success: You have modified Free Checkout payment module!';
-$_['text_edit']          = 'Edit Free Checkout';
+$_['text_extension']     = 'Extensiones';
+$_['text_success']       = 'Éxito: ¡Ha modificado el módulo de Pago Gratis!';
+$_['text_edit']          = 'Editar Pago Gratis';
 
 // Entry
-$_['entry_order_status'] = 'Order Status';
-$_['entry_status']       = 'Status';
-$_['entry_sort_order']   = 'Sort Order';
+$_['entry_order_status'] = 'Estado del pedido';
+$_['entry_status']       = 'Estado';
+$_['entry_sort_order']   = 'Orden de aparición';
 
 // Error
-$_['error_permission']   = 'Warning: You do not have permission to modify payment Free Checkout!';
+$_['error_permission']   = 'Advertencia: ¡No tiene permiso para modificar el Pago Gratis!';
