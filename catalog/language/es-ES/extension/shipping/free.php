659b645f54bcfd67598201283e818ecf37d8ddb5
Rudwolfr <Rudwolfr@users.noreply.github.com>
2018-09-25 12:13:29 -0500

    Update free.php

--- catalog/language/es-ES/extension/shipping/free.php
+++ catalog/language/es-ES/extension/shipping/free.php
@@ -1,4 +1,4 @@
 <?php
 // Text
-$_['text_title']       = 'Free Shipping';
-$_['text_description'] = 'Free Shipping';
+$_['text_title']       = 'Envío Gratis';
+$_['text_description'] = 'Envío Gratis';
