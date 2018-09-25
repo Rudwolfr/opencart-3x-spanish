e262312578ca2eca7a949e90d1a3432ec0331d19
Rudwolfr <Rudwolfr@users.noreply.github.com>
2018-09-25 12:39:45 -0500

    Update free.php

--- admin/language/es-ES/extension/shipping/free.php
+++ admin/language/es-ES/extension/shipping/free.php
@@ -1,20 +1,20 @@
 <?php
 // Heading
-$_['heading_title']    = 'Free Shipping';
+$_['heading_title']    = 'Envío Gratis';
 
 // Text
-$_['text_extension']   = 'Extensions';
-$_['text_success']     = 'Success: You have modified free shipping!';
-$_['text_edit']        = 'Edit Free Shipping';
+$_['text_extension']   = 'Extensiones';
+$_['text_success']     = 'Éxito: ¡Ha modificado el envío gratis!';
+$_['text_edit']        = 'Editar Envío Gratis';
 
 // Entry
 $_['entry_total']      = 'Total';
-$_['entry_geo_zone']   = 'Geo Zone';
-$_['entry_status']     = 'Status';
-$_['entry_sort_order'] = 'Sort Order';
+$_['entry_geo_zone']   = 'Geo Zona';
+$_['entry_status']     = 'Estado';
+$_['entry_sort_order'] = 'Orden del Pedido';
 
 // Help
-$_['help_total']       = 'Sub-Total amount needed before the free shipping module becomes available.';
+$_['help_total']       = 'Cantidad del Sub-Total que se necesita para que el envío gratis esté disponible.';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify free shipping!';
+$_['error_permission'] = 'Advertencia: ¡No tiene permiso para modificar envío gratis!';
