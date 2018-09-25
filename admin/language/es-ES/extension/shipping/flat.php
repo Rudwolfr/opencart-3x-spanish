20df4600332f5ba771f4c5483e0046eb7263511e
Rudwolfr <Rudwolfr@users.noreply.github.com>
2018-09-25 12:42:08 -0500

    Update flat.php

--- admin/language/es-ES/extension/shipping/flat.php
+++ admin/language/es-ES/extension/shipping/flat.php
@@ -1,18 +1,18 @@
 <?php
 // Heading
-$_['heading_title']    = 'Flat Rate';
+$_['heading_title']    = 'Tarifa Fija';
 
 // Text
-$_['text_extension']   = 'Extensions';
-$_['text_success']     = 'Success: You have modified flat rate shipping!';
-$_['text_edit']        = 'Edit Flat Rate Shipping';
+$_['text_extension']   = 'Extensiones';
+$_['text_success']     = 'Éxito: ¡Ha modificado la tarifa fija!';
+$_['text_edit']        = 'Editar Tarifa Fija';
 
 // Entry
-$_['entry_cost']       = 'Cost';
-$_['entry_tax_class']  = 'Tax Class';
-$_['entry_geo_zone']   = 'Geo Zone';
-$_['entry_status']     = 'Status';
-$_['entry_sort_order'] = 'Sort Order';
+$_['entry_cost']       = 'Costo';
+$_['entry_tax_class']  = 'Clase de Impuestos';
+$_['entry_geo_zone']   = 'Geo Zona';
+$_['entry_status']     = 'Estado';
+$_['entry_sort_order'] = 'Orden del Pedido';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify flat rate shipping!';
+$_['error_permission'] = 'Advertencia: ¡No tiene permiso para modificar tarifa fija!';
