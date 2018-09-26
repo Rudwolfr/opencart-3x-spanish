95d7dfcae1fd6afd08c0cd46a326a834fcbcef53
Rudwolfr <Rudwolfr@users.noreply.github.com>
2018-09-26 10:05:34 -0500

    Update google.php

--- catalog/language/es-ES/extension/captcha/google.php
+++ catalog/language/es-ES/extension/captcha/google.php
@@ -3,7 +3,7 @@
 $_['text_captcha']  = 'Captcha';
 
 // Entry
-$_['entry_captcha'] = 'Please complete the captcha validation below';
+$_['entry_captcha'] = 'Por favor completa la validación del captcha abajo';
 
 // Error
-$_['error_captcha'] = 'Verification is not correct!';
+$_['error_captcha'] = '¡La verificación no es correcta!';
