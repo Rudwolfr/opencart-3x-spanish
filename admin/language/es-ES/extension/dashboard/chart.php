5a456b8ea8f9fb3b6191848cb1aa825b4ce66720
Rudwolfr <Rudwolfr@users.noreply.github.com>
2018-09-26 12:26:23 -0500

    Update chart.php

--- admin/language/es-ES/extension/dashboard/chart.php
+++ admin/language/es-ES/extension/dashboard/chart.php
@@ -1,22 +1,22 @@
 <?php
 // Heading
-$_['heading_title']    = 'Sales Analytics';
+$_['heading_title']    = 'Análisis de Ventas';
 
 // Text
-$_['text_extension']   = 'Extensions';
-$_['text_success']     = 'Success: You have modified dashboard chart!';
-$_['text_edit']        = 'Edit Dashboard Chart';
-$_['text_order']       = 'Orders';
-$_['text_customer']    = 'Customers';
-$_['text_day']         = 'Today';
-$_['text_week']        = 'Week';
-$_['text_month']       = 'Month';
-$_['text_year']        = 'Year';
+$_['text_extension']   = 'Extensiones';
+$_['text_success']     = 'Éxito: ¡Ha modificado el cuadro del tablero!';
+$_['text_edit']        = 'Editar Cuadro del Tablero';
+$_['text_order']       = 'Ordenes';
+$_['text_customer']    = 'Clientes';
+$_['text_day']         = 'Hoy';
+$_['text_week']        = 'Semana';
+$_['text_month']       = 'Mes';
+$_['text_year']        = 'Año';
 
 // Entry
-$_['entry_status']     = 'Status';
-$_['entry_sort_order'] = 'Sort Order';
-$_['entry_width']      = 'Width';
+$_['entry_status']     = 'Estado';
+$_['entry_sort_order'] = 'Orden del Pedido';
+$_['entry_width']      = 'Ancho';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify dashboard chart!';
+$_['error_permission'] = 'Advertencia: ¡No tiene permisos para modificar el cuadro de tablero!';
