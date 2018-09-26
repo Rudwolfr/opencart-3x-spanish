f8f4abffbc34e74644c4f0505fafd80dc2055114
Rudwolfr <Rudwolfr@users.noreply.github.com>
2018-09-26 12:23:02 -0500

    Update online.php

--- admin/language/es-ES/extension/dashboard/online.php
+++ admin/language/es-ES/extension/dashboard/online.php
@@ -1,17 +1,17 @@
 <?php
 // Heading
-$_['heading_title']    = 'People Online';
+$_['heading_title']    = 'Gente en Línea';
 
 // Text
-$_['text_extension']   = 'Extensions';
-$_['text_success']     = 'Success: You have modified dashboard online!';
-$_['text_edit']        = 'Edit Dashboard Online';
-$_['text_view']        = 'View more...';
+$_['text_extension']   = 'Extensiones';
+$_['text_success']     = '¡Éxito: Ha modificado el tablero en línea!';
+$_['text_edit']        = 'Editar Tablero en Línea';
+$_['text_view']        = 'Ver más...';
 
 // Entry
-$_['entry_status']     = 'Status';
-$_['entry_sort_order'] = 'Sort Order';
-$_['entry_width']      = 'Width';
+$_['entry_status']     = 'Estado';
+$_['entry_sort_order'] = 'Orden del Pedido';
+$_['entry_width']      = 'Ancho';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify dashboard online!';
+$_['error_permission'] = 'Advertencia: ¡No tiene permiso para modificar el trablero en línea!';
